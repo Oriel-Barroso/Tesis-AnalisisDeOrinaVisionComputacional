e6bc33f7e949f917feb60486dc8c347596118459
Oriel-Barroso <oriel_gold97@outlook.com>
2023-06-02 13:36:17 -0300

    prueba

--- codigos/prueba.py
+++
@@ -1,9 +0,0 @@
-lista = ['1','2','3']
-dic = {'a1':1, 'a': 2, 'b2': 3, 'ggg5': 4}
-dic2 = {}
-for k,v in dic.items():
-    print(k[-1])
-    if k[-1] in lista:
-        dic2[k] = v
-
-print(dic2)
--- codigos/prueba1.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:403e62feaf793255607af85e66abec90fd658a63b2935f712eb533d70b93f040
-size 55
-=======
-img = r'C:\Users\Admin\Documents\Images\Dataset\Test'
->>>>>>> dd70e7b7faf0d5343b65a4259fa25ca263dd82da
